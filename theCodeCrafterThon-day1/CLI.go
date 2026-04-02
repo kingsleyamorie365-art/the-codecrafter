22642c59bead12501429b4df90d5531b484c02a6
kingsleyamorie365-art <kingsleyamorie365@gmail.com>
2026-04-02 17:46:01 +0100

    Cli-calculator project

--- theCodeCrafterThon-day1/CLI.go
+++ theCodeCrafterThon-day1/CLI.go
@@ -3,37 +3,41 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"time"
 	"strconv"
+	"time"
 )
+
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }
-func getValidInput(opt string) (float64, error) {
+func getValidInput(opt string) float64 {
 	var log string
 	for {
-		fmt.Println(opt)
-		fmt.Println(&log)
-		value, err := strconv.ParseFloat(log, 64)
-		if err != nil {
-			fmt.Println("Invalid Input. Please input Numbers.")
-			continue
+		fmt.Print(opt)
+		fmt.Scan(&log)
+
+		if value, err := strconv.ParseInt(log, 0, 64); err == nil {
+			return float64(value)
 		}
-		return value, nil
+		if value, err := strconv.ParseFloat(log, 64); err == nil {
+			return value
+		}
+		fmt.Println("Invalid Input. PLease input Numbers.")
 	}
 }
 
-func cliCalculator(){
+func cliCalculator() {
 	var option int
+	var choice int
 
-	greetMessages := []string {
+	greetMessages := []string{
 		"Welcome on board!",
 		"Hey, there!",
 		"Welcome back, champ!",
 		"Today's looking warm, man!",
 		"Look who we have here",
 	}
-	
+
 	fmt.Println(greetMessages[rand.Intn(len(greetMessages))])
 
 	fmt.Println()
@@ -44,41 +48,82 @@ start1:
 	if option == 1 {
 		fmt.Println("Access granted!")
 	} else if option != 1 {
-		
+
 		fmt.Println("Error Found!...Wrong Input Enter 1 to proceed_]")
 		goto start1
 	}
-
 	var name string
 	fmt.Println()
 	fmt.Println("[Welcome To Simplified Calculator]")
 	fmt.Println()
 	fmt.Print("Enter Name: ")
 	fmt.Scanln(&name)
-	fmt.Printf("\nWelcome %s To CLI-CALCULATOR", name)
+	fmt.Printf("\nHey %s Welcome To CLI-CALCULATOR", name)
 	fmt.Println()
+start2:
+
+    fmt.Println()
 	fmt.Println("SELECT OPERATION")
 	fmt.Println("1. Addition")
 	fmt.Println("2. Subtraction")
 	fmt.Println("3. Multiplication")
 	fmt.Println("4. Division")
 	fmt.Println("0. Close - Quit program!")
-	fmt.Println("#. HELP")
+	fmt.Println("10. HELP")
 	fmt.Println("")
-
-	var choice int
 	fmt.Scanln(&choice)
 
-	for {
-		switch choice {
-		case 1:
-			Num1 := getValidInput("Enter first number: ")
-			Num2 := getValidInput("Enter second number: ")
-			fmt.Printf("%g + %g = %g", Num1, Num2, Num1 + Num2)
-		}
+	// if choice == 1 || choice == 2 || choice == 3 || choice == 4 || choice == 0 || choice == 00 {
+	// 	return
+	// }else if choice != 1 || choice != 2 || choice != 3 || choice != 4 || choice != 0 || choice != 00 {
+	// 	fmt.Println("Invalid command: Press # for help ")
+	// }else {
+	// 	// if err != nil {
+	// 	// 	return 
+	// 	// }
+	// 	fmt.Println("Error: Input Number")
+	// 	goto start2
+	// }
+
+	switch choice {
+	case 1:
+		num1 := getValidInput("Enter first number: ")
+		num2 := getValidInput("Enter second number: ")
+		fmt.Printf("%g + %g = %g\n", num1, num2, num1+num2)
+		goto start2
+
+	case 2:
+		num1 := getValidInput("Enter first number: ")
+		num2 := getValidInput("Enter second number: ")
+		fmt.Printf("%g - %g = %g\n", num1, num2, num1-num2)
+		goto start2
+
+	case 3:
+		num1 := getValidInput("Enter first number: ")
+		num2 := getValidInput("Enter second number: ")
+		fmt.Printf("%g * %g = %g\n", num1, num2, num1*num2)
+		goto start2
+		
+	case 4:
+		num1 := getValidInput("Enter first number: ")
+		num2 := getValidInput("Enter second number: ")
+		fmt.Printf("%g / %g = %g\n", num1, num2, num1/num2)
+		goto start2
+
+	case 0:
+		fmt.Println()
+		fmt.Println("Good bye, champ!")
+
+	default:
+		fmt.Println()
+		fmt.Println("Unknown command!. Press 10 for HELP")
+		fmt.Println()
+		goto start2
+
 	}
-	
+
 }
-func main(){
+func main() {
+
 	cliCalculator()
 }
